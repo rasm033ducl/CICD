48aae44fc30b1cf98338dad3677fc8d788c79525
rasm033ducl <150004942+rasm033ducl@users.noreply.github.com>
2023-11-06 09:23:37 +0100

    csharp
    csharp

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	setupFiles: [
-		'<rootDir>/tests/js/setupTests.js',
-	],
-	testMatch: [
-		'**/tests/js/**/*.test.js',
-	],
-};
--- src/js/functions.js
+++
@@ -1,7 +0,0 @@
-/**
- * Return the answer to life, universe and everything.
- * @return {string} The answer to life, universe and everything.
- */
-export function getAnswer() {
-	return '42';
-}
